2623fbb9431c77fb535f7ef8a776250ed7d0afe4
likui628 <likui.it@gmail.com>
2022-06-26 15:08:25 +0800

    feat: add vue-loader

--- src/index.js
+++
@@ -1,9 +0,0 @@
-function component() {
-  const element = document.createElement("div");
-
-  element.innerHTML = "Hello Webpack";
-
-  return element;
-}
-
-document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,37 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/main.js",
   mode: "production",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+    new VueLoaderPlugin(),
+  ],
 };
