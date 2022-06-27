a981cd0758b8922b83380d24b5ce141bb8a1841d
likui628 <likui.it@gmail.com>
2022-06-27 21:48:12 +0800

    chore: add clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
@@ -55,6 +56,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "index.html",
     }),
