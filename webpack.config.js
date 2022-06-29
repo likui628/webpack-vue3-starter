9aab8a7108cbd8858e9e37d75ff89a7dbc53df15
likui628 <likui.it@gmail.com>
2022-06-29 10:27:07 +0800

    feat: MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,61 +2,72 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const { VueLoaderPlugin } = require("vue-loader");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: "./src/main.ts",
-  mode: "production",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash:8].js",
-  },
-  devServer: {
-    port: 3000,
-    hot: true,
-    open: true,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+module.exports = (env) => {
+  const isProd = env.production;
+
+  return {
+    entry: "./src/main.ts",
+    mode: "production",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].[contenthash:8].js",
     },
-    // Add `.ts` as a resolvable extension.
-    extensions: [".ts", ".js"],
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
+    devServer: {
+      port: 3000,
+      hot: true,
+      open: true,
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: "vue-loader",
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
       },
-      {
-        test: /\.css$/,
-        use: ["vue-style-loader", "css-loader"],
+      // Add `.ts` as a resolvable extension.
+      extensions: [".ts", ".js"],
+    },
+    optimization: {
+      splitChunks: {
+        chunks: "all",
       },
-      {
-        test: /\.less$/,
-        use: ["vue-style-loader", "css-loader", "less-loader"],
-      },
-      {
-        test: /\.ts$/,
-        loader: "ts-loader",
-        options: { appendTsSuffixTo: [/\.vue$/] },
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: "vue-loader",
+        },
+        {
+          test: /\.ts$/,
+          loader: "ts-loader",
+          options: { appendTsSuffixTo: [/\.vue$/] },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : "vue-style-loader",
+            "css-loader",
+          ],
+        },
+        {
+          test: /\.less$/,
+          use: ["vue-style-loader", "css-loader", "less-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "index.html",
+      }),
+      new VueLoaderPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      }),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
-    new VueLoaderPlugin(),
-  ],
+  };
 };
