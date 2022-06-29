ed0348c6cd133408ce3d337c0417cc7d28e5b321
likui628 <likui.it@gmail.com>
2022-06-29 18:58:10 +0800

    feat: eslint

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,13 @@ import "./style.css"
 
 <template>
   <div class="main">
-    <img alt="Vue.js logo" src="./assets/logo.png" decoding="async" width="215" height="186" />
+    <image
+      alt="Vue.js logo" 
+      src="./assets/logo.png" 
+      decoding="async" 
+      width="215" 
+      height="186" 
+    />
     <HelloWorld />
-
   </div>
 </template>
